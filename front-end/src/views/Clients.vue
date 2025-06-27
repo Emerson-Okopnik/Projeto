973293a87e7e13525fde2a8a370393695370edd1
Emerson-Okopnik <emer00k@gmail.com>
2025-06-27 10:23:23 -0300

    teste deploy automatico

--- front-end/src/views/Clients.vue
+++ front-end/src/views/Clients.vue
@@ -13,7 +13,7 @@
           type="button"
           class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
         >
-          Novo Cliente
+          Novo Cliente ///
         </button>
       </div>
     </div>
